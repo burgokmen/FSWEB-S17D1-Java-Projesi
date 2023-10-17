dc3319f575d4badd26cc6b91eff13affee7bd5f2
burgokmen <burgokmen@gmail.com>
2023-10-17 23:04:22 +0300

    animal comp ongoing

---
+++ src/main/java/com/brutech/spring/controller/AnimalController.java
@@ -0,0 +1,12 @@
+package com.brutech.spring.controller;
+
+import com.brutech.spring.entity.Animal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnimalController {
+   private Map<Integer, Animal> animals = new HashMap<>();
+
+
+}
