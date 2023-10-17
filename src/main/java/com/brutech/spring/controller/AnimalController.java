564780e1997ab7e3eb67dae2dd1f43cafc4dfe99
burgokmen <burgokmen@gmail.com>
2023-10-17 23:31:00 +0300

    animal controller ongoing

--- src/main/java/com/brutech/spring/controller/AnimalController.java
+++ src/main/java/com/brutech/spring/controller/AnimalController.java
@@ -1,14 +1,41 @@
 package com.brutech.spring.controller;
 
 import com.brutech.spring.entity.Animal;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/animal")
 public class AnimalController {
    private Map<Integer, Animal> animals = new HashMap<>();
+
+   @PostConstruct
+   public void init() {
+       animals = new HashMap<>();
+   }
+
+   @GetMapping("/")
+   public List<Animal> findAll() {
+       return animals.values().stream().toList();
+   }
+
+    @GetMapping("/{id}")
+   public Animal find(@PathVariable Integer id) {
+       return animals.get(id);
+   }
+
+   @PostMapping("/")
+   public Animal save(@RequestBody Animal animal) {
+       animals.put(animal.getId(), animal);
+       return animals.get(animal.getId());
+   }
+
+
+
 
 
 }
