49ded4d7b6916ef21168c1cf49073995f75cb044
burgokmen <burgokmen@gmail.com>
2023-10-17 23:59:27 +0300

    animal validation ongoing

--- src/main/java/com/brutech/spring/validation/AnimalValidation.java
+++ src/main/java/com/brutech/spring/validation/AnimalValidation.java
@@ -1,9 +1,21 @@
 package com.brutech.spring.validation;
 
+import com.brutech.spring.entity.Animal;
+
+import java.util.Map;
+
 public class AnimalValidation {
     public static boolean isIdValid(int id) {
         return id > 0;
     }
 
+    public static boolean isAnimalContains(Map<Integer, Animal> animals, int id) {
+        return animals.containsKey(id);
+    }
 
+    public static boolean isAnimalCredentialsValid(Animal animal){
+        return animal.getId() > 0 &&
+                animal.getName() != null && !animal.getName().isEmpty() &&
+                animal.getWeight() > 0;
+    }
 }
