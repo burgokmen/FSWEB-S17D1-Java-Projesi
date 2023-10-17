144bb6761a085978c26f0b38ab9ed1036f771f96
burgokmen <burgokmen@gmail.com>
2023-10-17 23:06:13 +0300

    controller animal

--- src/main/java/com/brutech/spring/controller/AnimalController.java
+++ src/main/java/com/brutech/spring/controller/AnimalController.java
@@ -1,10 +1,12 @@
 package com.brutech.spring.controller;
 
 import com.brutech.spring.entity.Animal;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class AnimalController {
    private Map<Integer, Animal> animals = new HashMap<>();
 
